251dda16d40bccae1451615e6404d0686a8c3d7c
minus43 <minu425854@gmail.com>
2025-03-24 17:04:13 +0900

    1-05 프롭스

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,43 @@
 import style from "./style";
+const topics = [
+  { id: 1, title: "HTML", body: "HTML is a markup language" },
+  { id: 2, title: "CSS", body: "CSS is a style sheet language" },
+  { id: 3, title: "JavaScript", body: "JavaScript is a scripting language" },
+];
 
-function Header() {
+
+function Header(props: { title: string }) {
   return (
     <header className={style.header}>
-      <h1 className={style.h1}>Hello World</h1>
+      <h1 className={style.h1}>{props.title}</h1>
     </header>
   );
 }
 
-function Nav() {
+function Nav(props: { topics: { id: number, title: string, body: string }[] }) {
+  const list = []
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i]
+    list.push(<li key={t.id}>
+      <a href={`/read/${t.id}`}>{t.title}</a>
+    </li>)
+  }
   return (
     <nav className={style.nav}>
       <ul className={style.ul}>
         <ol className={style.ol}>
-          <li className={style.li}><a href="/read/1">HTML</a></li>
-          <li className={style.li}><a href="/read/2">CSS</a></li>
-          <li className={style.li}><a href="/read/3">JavaScript</a></li>
+          {list}
         </ol>
       </ul>
     </nav>
   );
 }
 
-function Article() {
+function Article(props: { title: string, body: string }) {
   return (
     <article className={style.article}>
-      <h2 className={style.h2}>Welcome</h2>
-      Hello, WEB
+      <h2 className={style.h2}>{props.title}</h2>
+      {props.body}
     </article>
   );
 }
@@ -34,9 +45,10 @@ function Article() {
 export default function Home() {
   return (
     <div className={style.div}>
-      <Header />
-      <Nav />
-      <Article />
+      <Header title="React" />
+      <Nav topics={topics} />
+      <Article title="Welcome" body="Hello, WEB" />
+      <Article title="Hi" body="Hello, React" />
     </div>
   );
 }
