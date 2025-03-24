c1b28107172bf30f9bbe7ab992cef6972273b423
minus43 <minu425854@gmail.com>
2025-03-24 16:48:37 +0900

    1-04 컴포넌트 만들기

---
+++ src/app/style.tsx
@@ -0,0 +1,15 @@
+const style = {
+  div: "min-h-screen",
+  header: "py-6", 
+  h1: "text-4xl font-bold",
+  nav: "p-4",
+  ul: "list-none p-0 m-0",
+  ol: "list-decimal pl-8",
+  li: "my-2",
+  a: "hover:underline",
+  article: "prose prose-slate max-w-none w-full mx-auto p-6 dark:prose-invert",
+  h2: "text-2xl font-semibold mb-4"
+};
+
+export default style;
+
